1ec00f7591dd12ed1c2db0afa53fe4dd42a18b19
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-04-29 22:07:18 +0200

    Edit script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const txtArea = document.querySelector('.text-area');
 const btnContainer = document.querySelector('.button-container');
-let btnStart = document.querySelector('.btn-start');
 let playerScore = 0;
 let cpuScore = 0;
 
 /* Click the button to start the game */
+let btnStart = document.querySelector('.button-start');
 btnStart.addEventListener('click', startGame);
 
 function getComputerChoice(){
@@ -83,120 +83,155 @@ function playOneRound(playerOption,cpuOption){
    txtArea.appendChild(scoreP);
 }
 
-function startGame(){
-   let playerChoice;
-   let cpuChoice;
+function resetGameContainer(i){
+   //Select all p markups in '.text-area'
    let ps = document.querySelectorAll('.text-area > p');
-   let buttons = document.querySelectorAll('.button-container > button');
-
    ps.forEach(p => {
       p.remove();
    });
+   //Select all buttons in '.button-container'
+   let buttons = document.querySelectorAll('button');
    buttons.forEach(button => {
       button.remove();
    });
+   btnContainer.setAttribute('id', 'round'+i);
 
+   // Create 'rock' button
    let btnRock = document.createElement('button');
-   btnRock.classList.add('btn-rock');
+   btnRock.classList.add('button-rock');
    btnRock.setAttribute("data-option", "ROCK");
+   btnRock.setAttribute("id","btn-rd"+i);
    btnRock.textContent = 'ROCK';
    btnContainer.appendChild(btnRock);
-
+   //Create 'paper' button
    let btnPaper = document.createElement('button');
-   btnPaper.classList.add('btn-paper');
+   btnPaper.classList.add('button-paper');
    btnPaper.setAttribute("data-option", "PAPER");
+   btnPaper.setAttribute("id","btn-rd"+i);
    btnPaper.textContent = 'PAPER';
    btnContainer.appendChild(btnPaper);
-
+   //Create 'scissors button
    let btnScissors = document.createElement('button');
-   btnScissors.classList.add('btn-scissors');
+   btnScissors.classList.add('button-scissors');
    btnScissors.setAttribute("data-option", "SCISSORS");
+   btnScissors.setAttribute("id","btn-rd"+i);
    btnScissors.textContent = 'SCISSORS';
    btnContainer.appendChild(btnScissors);
+}
+function round1(){
+   resetGameContainer(2);
+   let playerChoice = this.getAttribute('data-option');
+   let cpuChoice = getComputerChoice();
+   playOneRound(playerChoice,cpuChoice);
 
-   let round1 = document.createElement('p');
-   round1.innerHTML = "Round 1 !<br>Choose your Move!";
-   txtArea.appendChild(round1);
+   let p = document.createElement('p');
+   p.innerHTML = "Round 2 !<br/>Choose your Move!";
+   txtArea.appendChild(p);
 
-   let roundOneButtons = document.querySelectorAll('button');
-   roundOneButtons.forEach(roundOnebutton => {
-      roundOnebutton.addEventListener('click',function(){
-         playerChoice = this.getAttribute('data-option');
-         cpuChoice = getComputerChoice();
-         playOneRound(playerChoice,cpuChoice);
-         let round2 = document.createElement('p');
-         round2.innerHTML = "Round 2 !<br/>Choose your Move!";
-         txtArea.appendChild(round2);
+   let roundButtons = document.querySelectorAll('#btn-rd'+2);
+   roundButtons.forEach(roundButton => {
+      roundButton.addEventListener('click',round2);
+   });
+}
 
-         let roundTwoButtons = document.querySelectorAll('button');
-         roundTwoButtons.forEach(roundTwobutton => {
-            roundTwobutton.addEventListener('click',function(){
-               playerChoice = this.getAttribute('data-option');
-               cpuChoice = getComputerChoice();
-               playOneRound(playerChoice,cpuChoice);
-               let round3 = document.createElement('p');
-               round3.innerHTML = "Round 3 !<br/>Choose your Move!";
-               txtArea.appendChild(round3);
+function round2(){
+   resetGameContainer(3);
+   let playerChoice = this.getAttribute('data-option');
+   let cpuChoice = getComputerChoice();
+   playOneRound(playerChoice,cpuChoice);
 
-               let roundThreeButtons = document.querySelectorAll('button');
-               roundThreeButtons.forEach(roundThreeButton => {
-                  roundThreeButton.addEventListener('click',function(){
-                     playerChoice = this.getAttribute('data-option');
-                     cpuChoice = getComputerChoice();
-                     playOneRound(playerChoice,cpuChoice);
-                     let round4 = document.createElement('p');
-                     round4.innerHTML = "Round 4 !<br/>Choose your Move!";
-                     txtArea.appendChild(round4);
+   let p = document.createElement('p');
+   p.innerHTML = "Round 3 !<br/>Choose your Move!";
+   txtArea.appendChild(p);
 
-                     let roundFourButtons = document.querySelectorAll('button');
-                     roundFourButtons.forEach(roundThreeButton => {
-                        roundThreeButton.addEventListener('click',function(){
-                           playerChoice = this.getAttribute('data-option');
-                           cpuChoice = getComputerChoice();
-                           playOneRound(playerChoice,cpuChoice);
-                           let round5 = document.createElement('p');
-                           round5.innerHTML = "Round 5 !<br/>Choose your Move!";
-                           txtArea.appendChild(round5);
+   let roundButtons = document.querySelectorAll('#btn-rd'+3);
+   roundButtons.forEach(roundButton => {
+      roundButton.addEventListener('click',round3);
+   });
+}
 
-                           let roundFiveButtons = document.querySelectorAll('button');
-                           roundFiveButtons.forEach(roundThreeButton => {
-                              roundThreeButton.addEventListener('click',function(){
-                                 playerChoice = this.getAttribute('data-option');
-                                 cpuChoice = getComputerChoice();
-                                 playOneRound(playerChoice,cpuChoice);
-                              });
-                           });
-                        });
-                     });
-                  });
-               });
-            });
-         });
-         //getGameOver();
-      });
+function round3(){
+   resetGameContainer(4);
+   let playerChoice = this.getAttribute('data-option');
+   let cpuChoice = getComputerChoice();
+   playOneRound(playerChoice,cpuChoice);
+
+   let p = document.createElement('p');
+   p.innerHTML = "Round 4 !<br/>Choose your Move!";
+   txtArea.appendChild(p);
+
+   let roundButtons = document.querySelectorAll('#btn-rd'+4);
+   roundButtons.forEach(roundButton => {
+      roundButton.addEventListener('click',round4);
+   });
+}
+
+function round4(){
+   resetGameContainer(5);
+   let playerChoice = this.getAttribute('data-option');
+   let cpuChoice = getComputerChoice();
+   playOneRound(playerChoice,cpuChoice);
+
+   let p = document.createElement('p');
+   p.innerHTML = "Round 5 !<br/>Choose your Move!";
+   txtArea.appendChild(p);
+
+   let roundButtons = document.querySelectorAll('#btn-rd'+5);
+   roundButtons.forEach(roundButton => {
+      roundButton.addEventListener('click',round5);
+   });
+}
+
+function round5(){
+   resetGameContainer('-final');
+   let playerChoice = this.getAttribute('data-option');
+   let cpuChoice = getComputerChoice();
+   playOneRound(playerChoice,cpuChoice);
+
+   let finalBtns = document.querySelectorAll('.button-container');
+   finalBtns.forEach(finalBtn=>{
+      finalBtn.remove();
+   });
+
+   getGameOver();
+}
+
+function startGame(){
+   resetGameContainer(1);
+
+   let p = document.createElement('p');
+   p.innerHTML = "Round 1 !<br>Choose your Move!";
+   txtArea.appendChild(p);
+
+   let roundButtons = document.querySelectorAll('button');
+   roundButtons.forEach(roundButton => {
+      roundButton.addEventListener('click',round1);
    });
 }
 
 function getGameOver(){
    let newP = document.createElement('p');
-   let btns = document.querySelectorAll('.button-container > button');
    txtArea.appendChild(newP);
+   
+   //IT'S A TIE
    if(playerScore==cpuScore){
       newP.textContent = "IT'S A TIE! You have to play again!";
       txtArea.appendChild(newP);
-      startGame();
+
+      /* RESTART THE GAME */ 
+
    }   
    else { 
+      //YOU WIN
       if(playerScore>cpuScore){
          newP.textContent = "YOU WIN! CONGRATULATION! THE EARTH IS SAFE NOW";
          txtArea.appendChild(newP);
-         btns.remove();
          return;
       }
+      //YOU LOSE
       else{
          newP.textContent = "YOU LOSE! EVERYONE IS FUCKING DEAD!!!";
          txtArea.appendChild(newP);
-         btns.remove();
          return;
       } 
    }
